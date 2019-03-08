27a958f7ebc0521e85787de15b3c86648edf880f
Ruochen Liu <ruochen99@berkeley.edu>
2019-03-08 15:37:46 -0800

    server

--- Chloe 02-18-19/api/server.py
+++ Chloe 02-18-19/api/server.py
@@ -7,6 +7,7 @@ import hashlib
 import pandas as pd
 from werkzeug.utils import secure_filename
 import pickle
+from Model import clean_cols
 
 
 #This is not but-free, still have some problems...
@@ -24,25 +25,23 @@ def process(pandas_dataset, dataframe):
             headers = list(dataset_df.columns.values)
             headers = clean_cols(headers)
             tags = list(dataset_df.iloc[0,:])
-            for i in range(len(headers)):
-                try:
-                    splitted = re.split('[(^\s+)+#]', tags[i])
-                    splitted = list(filter(None, splitted))
-                    hashtag = splitted[0]
-                    attributes = splitted[1:]
-                    dic = {'Header': headers[i], 'Tag': hashtag, 'Attributes': attributes, 
-                           'Data': list(dataset_df.iloc[1:, i]), 
-                           'Relative Column Position': (i+1) / len(dataset_df.columns), 
-                           'Dataset_name': os.path.basename(path), 
-                           'Organization': organization,
-                           'Index': index}
-                    dataframe.loc[len(dataframe)] = dic
-                except:
-                    print("Error: different number of headers and tags")
-            count += 1
-        os.remove(path)
-        print("File Removed!")
-        return
+    for i in range(len(headers)):
+        try:
+            splitted = re.split('[(^\s+)+#]', tags[i])
+            splitted = list(filter(None, splitted))
+            hashtag = splitted[0]
+            attributes = splitted[1:]
+            dic = {'Header': headers[i], 'Tag': hashtag, 'Attributes': attributes, 
+                   'Data': list(dataset_df.iloc[1:, i]), 
+                   'Relative Column Position': (i+1) / len(dataset_df.columns), 
+                   'Dataset_name': os.path.basename(path), 
+                   'Organization': organization,
+                   'Index': index}
+            dataframe.loc[len(dataframe)] = dic
+        except:
+            print("Error: different number of headers and tags")
+    count += 1
+    return
 
 
 def allowed_file(filename):
@@ -59,7 +58,7 @@ def upload_file():
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
-        file.save(os.getcwd())
+        # file.save(os.getcwd())
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
@@ -67,11 +66,13 @@ def upload_file():
             filename = secure_filename(file.filename)
             input_dataset = pd.read_csv(file)
                 # process the untagged dataset
-            
-            process(input_dataset, )
-            model = pickle.load(open("model.pkl", "rb"))
-            output_dataset = model.predict(input_dataset.values.to_list())
+            count = 0
+            col_names = ['Header', 'Tag', 'Attributes','Data','Relative Column Position','Dataset_name', 'Organization','Index']
+            headers_and_tags= pd.DataFrame(columns = col_names)
 
+            process(input_dataset, headers_and_tags)
+            model = pickle.load(open("model.pkl", "rb"))
+            output_dataset = model.predict(headers_and_tags.values.to_list())
 
             resp = make_response(output_dataset.to_csv())
             resp.headers["Content-Disposition"] = "attachment; filename=export.csv"
@@ -89,19 +90,19 @@ def upload_file():
 
  # action={{url_for('tag', dataset='df')}}   
 
-@app.route('/tag/<dataset>', methods=['POST'])
-def tag(dataset):
-	#getting our trained model	
-	model = pickle.load(open("model.pkl", "rb"))   	
-	input_dataset = request.get_json()
-	tags = model.predict(input_dataset).to_list()
-	#Add one row to the dataset
-	for i in range(len(tags)):
-		input_dataset.loc1[1: i] = tags[i]
-	response = {}
-	response['tags'] = tags
-	#returning the response object as json
-    return flask.jsonify(response)
+# @app.route('/tag/<dataset>', methods=['POST'])
+# def tag(dataset):
+# 	#getting our trained model	
+# 	model = pickle.load(open("model.pkl", "rb"))   	
+# 	input_dataset = request.get_json()
+# 	tags = model.predict(input_dataset).to_list()
+# 	#Add one row to the dataset
+# 	for i in range(len(tags)):
+# 		input_dataset.loc1[1: i] = tags[i]
+# 	response = {}
+# 	response['tags'] = tags
+# 	#returning the response object as json
+#     return flask.jsonify(response)
 
 if __name__ == '__main__':
      app.run(debug=True)
